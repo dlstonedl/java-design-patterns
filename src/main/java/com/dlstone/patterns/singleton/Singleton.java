654d39485b5b4fa41eeaeb418616f110dc4095b9
dlstonedl <ldeng@thoughtworks.com>
2019-03-19 23:06:08 +0800

    hungry singleton

--- src/main/java/com/dlstone/patterns/singleton/Singleton.java
+++ src/main/java/com/dlstone/patterns/singleton/Singleton.java
@@ -1,4 +1,12 @@
 package com.dlstone.patterns.singleton;
 
 public class Singleton {
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getSingleton() {
+        return singleton;
+    }
 }
