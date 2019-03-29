74bfad14c6aea5396eb1ae259bc1ab7710680439
dlstonedl <ldeng@thoughtworks.com>
2019-03-29 23:38:09 +0800

    remove no use code

--- src/main/java/com/dlstone/patterns/proxy/DynamicProxy.java
+++ src/main/java/com/dlstone/patterns/proxy/DynamicProxy.java
@@ -3,7 +3,7 @@ package com.dlstone.patterns.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
-public class DynamicProxy<T> {
+public class DynamicProxy {
     public static <T> T newProxyInstance(ClassLoader classLoader, Class<?>[] interfaces, InvocationHandler handler) {
         new BeforeAdvice().exec();
         return (T) Proxy.newProxyInstance(classLoader, interfaces, handler);
