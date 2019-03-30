b7f392bca77a3088b7422cfd897fca51197ff552
dlstonedl <ldeng@thoughtworks.com>
2019-03-30 22:48:31 +0800

    refactor

--- src/main/java/com/dlstone/patterns/factory_method/java8_new/HttpConnect.java
+++ src/main/java/com/dlstone/patterns/factory_method/java8_new/HttpConnect.java
@@ -2,6 +2,8 @@ package com.dlstone.patterns.factory_method.java8_new;
 
 public class HttpConnect implements Connect {
 
+    HttpConnect(){}
+
     @Override
     public void connect() {
         System.out.println("HttpConnect connect...");
--- src/main/java/com/dlstone/patterns/factory_method/java8_new/RpcConnect.java
+++ src/main/java/com/dlstone/patterns/factory_method/java8_new/RpcConnect.java
@@ -2,6 +2,8 @@ package com.dlstone.patterns.factory_method.java8_new;
 
 public class RpcConnect implements Connect {
 
+    RpcConnect(){}
+
     @Override
     public void connect() {
         System.out.println("RpcConnect connect...");
